c89ca19c61b750bac080526e2c8860189ab146f1
Agustin Santicchia <santicchiaagus85@gmail.com>
2025-01-19 16:05:43 -0300

    Add repositories for Marvel characters and comics

--- marvel-challenge/src/main/java/com/test/api/persistance/integration/marvel/repository/ComicRepository.java
+++ marvel-challenge/src/main/java/com/test/api/persistance/integration/marvel/repository/ComicRepository.java
@@ -8,11 +8,48 @@ import java.util.List;
 
 @Repository
 public class ComicRepository {
+
+    @Autowired
+    private MarvelAPIConfig marvelAPIConfig;
+    @Autowired
+    private HttpClientService httpClientService;
+
+    @Value("${integration.marvel.base-path}")
+    private String basePath;
+    private String comicPath;
+
+    @PostConstruct
+    private void setPath(){
+        comicPath = basePath.concat("/").concat("comics");
+    }
+
     public List<ComicDto> findAll(MyPageable pageable, Long characterId) {
-        return null;
+        Map<String, String> marvelQueryParams = getQueryParamsForFindAll(pageable, characterId);
+
+        JsonNode response = httpClientService.doGet(comicPath, marvelQueryParams, JsonNode.class);
+
+        return ComicMapper.toDtoList(response);
+    }
+
+    private Map<String, String> getQueryParamsForFindAll(MyPageable pageable, Long characterId) {
+        Map<String, String> marvelQueryParams = marvelAPIConfig.getAuthenticationQueryParams();
+
+        marvelQueryParams.put("offset", Long.toString(pageable.offset()));
+        marvelQueryParams.put("limit", Long.toString(pageable.limit()));
+
+        if(characterId != null && characterId.longValue() > 0){
+            marvelQueryParams.put("characters", Long.toString(characterId));
+        }
+
+        return marvelQueryParams;
     }
 
     public ComicDto findById(Long comicId) {
-        return null;
+        Map<String, String> marvelQueryParams = marvelAPIConfig.getAuthenticationQueryParams();
+
+        String finalUrl = comicPath.concat("/").concat(Long.toString(comicId));
+        JsonNode response = httpClientService.doGet(finalUrl, marvelQueryParams, JsonNode.class);
+
+        return ComicMapper.toDtoList(response).get(0);
     }
 }
